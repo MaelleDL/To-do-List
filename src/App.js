e1ee66ef4c4a5c24796b87bb2e984da13a95bb4f
MaelleDaniel <maelledaniel2@gmail.com>
2021-10-28 10:55:57 +0200

    personnalisation

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>My To-do list</h1>
+        <h1>To-do list</h1>
       </header>
       <Form
         inputText={inputText}
